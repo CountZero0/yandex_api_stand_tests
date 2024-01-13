fe621658aa79ff743be920a11511ebabb2b6f59a
Konstantin <akechijinsai@gmail.com>
2024-01-13 21:39:38 +0300

    123

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://2e95a622-0d44-4196-bb08-bfb8e6d70841.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
